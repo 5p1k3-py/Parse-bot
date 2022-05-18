4e2d6161c72acf4716be50d90971c29dbda40c60
Andreii <68230808+Valeriyovich@users.noreply.github.com>
2022-05-18 20:56:16 +0300

    Update main.py

--- parse/main.py
+++ parse/main.py
@@ -3,10 +3,10 @@ import csv
 from datetime import datetime
 from bs4 import BeautifulSoup
 
-headers = {
-    'user-agent':'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Mobile Safari/537.36',
-    'accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
-}
+# headers = {
+#     'user-agent':'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Mobile Safari/537.36',
+#     'accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
+# }
 
 def get_data(url):
     cur_date = datetime.now().strftime('%m_%d_%Y')
